2893670715908a9ce3227983c3f19b4726f4e410
oott0228 <oott0228@gmail.com>
2022-12-06 16:29:29 +0900

    first commit

---
+++ quiz/questions.js
@@ -0,0 +1,49 @@
+const answersList = document.querySelectorAll('ol.answers li');
+
+answersList.forEach(li => li.addEventListener('click', checkClickedAnswer));
+
+// 正しい答え
+const correctAnswers = {
+  question1: 'B',
+  question2: 'A',
+  question3: 'C',
+  question4: 'D',
+};
+
+
+function checkClickedAnswer(event) {
+  // クリックされた答えの要素
+  const clickedAnswerElement = event.currentTarget;
+
+  // 選択した答え
+  const selectedAnswer = clickedAnswerElement.dataset.answer;
+
+  const questionId = clickedAnswerElement.closest('ol.answers').dataset.id;
+  // 正しい答え
+  const correctAnswer = correctAnswers[questionId];
+
+  // メッセージを入れる変数を用意
+  let message;
+  // カラーコードを入れる変数を用意
+  let answerColorCode;
+
+  if (selectedAnswer === correctAnswer) {
+    // 正しい答えだったとき
+    message = '正解です！おめでとう';
+    answerColorCode = '';
+  } else {
+    // 間違った答えだったとき
+    message = '残念！不正解です';
+    answerColorCode = '#f05959';
+  }
+
+  // 
+  alert(message);
+
+  // 間違っていた時だけ色が変わる
+  document.querySelector('span#correct-answer').style.color = answerColorCode;
+
+  // 答え全体を表示
+  document.querySelector('div#section-correct-answer').style.display = 'block';
+}
+
