2315c8a7b24aa93c0c0bb60509578ff45eb013d0
oott0228 <oott0228@gmail.com>
2023-01-07 20:55:30 +0900

    mysqlの設定変更

--- example-app/app/Http/Controllers/QuizController.php
+++ example-app/app/Http/Controllers/QuizController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quiz;
 use Illuminate\Http\Request;
 
 class QuizController extends Controller
@@ -36,7 +37,7 @@ class QuizController extends Controller
     {
         // 入力内容のチェック
         // ルールに一致しない入力の場合は、自動的に入力画面を表示させる
-        $request->validate([
+        $validatedData = $request->validate([
             'question' => 'required|max:255',
             'answer_a' => 'required|max:255',
             'answer_b' => 'required|max:255',
@@ -45,7 +46,23 @@ class QuizController extends Controller
             'correct_answer' => 'required|in:A,B,C,D',
             'explanation' => 'max:65535',
         ]);
-        return view('quizzes.index');
+
+        // Modelを作成
+        $Quiz = new Quiz;
+        $Quiz->question = $validatedData['question'];
+        $Quiz->answer_a = $validatedData['answer_a'];
+        $Quiz->answer_b = $validatedData['answer_b'];
+        $Quiz->answer_c = $validatedData['answer_c'];
+        $Quiz->answer_d = $validatedData['answer_d'];
+        $Quiz->correct_answer = $validatedData['correct_answer'];
+        $Quiz->explanation = $validatedData['explanation'];
+
+        // ModelをDBに保存
+        $Quiz->save();
+
+        // 一覧ページを表示
+        // リロードされた時にもう一度データが保存されないようにリダイレクトさせる
+        return redirect(route('quizzes.index'));
     }
 
     /**
